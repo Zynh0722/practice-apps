a0f3fdc2a3a7f1ffbd3c24b436f90f58221a46a7
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-23 11:11:26 -0700

    Use .then to ensure connection isn't closed early

--- 1-glossary/dev/populateDB.js
+++ 1-glossary/dev/populateDB.js
@@ -3,6 +3,6 @@ let testData = require('./testData.json');
 
 Entry.insertMany(testData.map(entry => new Entry(entry)))
   .then(res => console.log(res))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
+  .then(() => mongoose.connection.close());
 
-mongoose.connection.close();
