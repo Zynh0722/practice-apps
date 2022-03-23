ff70643e3d2c978d69a5f5ced547691a8e650b56
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-23 10:08:31 -0700

    Make DB schema and model, then export them

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,19 @@
 const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+let entrySchema = mongoose.schema({
+  word: String,
+  definition: String
+});
+
+let Entry = mongoose.model('Entry', entrySchema);
+
+module.exports.Entry = Entry;
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
