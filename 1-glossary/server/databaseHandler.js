ad2cb98f8f44514cee630369c10d9162210fdb5f
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-24 16:40:30 -0700

    Number cast pageSize so it works

--- 1-glossary/server/databaseHandler.js
+++ 1-glossary/server/databaseHandler.js
@@ -5,7 +5,7 @@ module.exports = {
 
   updateEntry: (_id, {word, definition}) => Entry.update({_id}, {word, definition}),
   
-  getEntries: ({page = 0, pageSize = 400}) => Entry.find({}).skip(page * pageSize).limit(pageSize).exec(),
+  getEntries: ({page = 0, pageSize = 10}) => Entry.find({}).skip(page * Number(pageSize)).limit(Number(pageSize)).exec(),
 
   deleteEntry: (_id) => Entry.deleteOne({_id})
 }
